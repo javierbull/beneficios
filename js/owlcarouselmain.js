f57aa8ce19881eeb06f46bc2d680afcd8a041833
javierbull <javierbullreyes@gmail.com>
2023-11-01 12:06:16 -0500

    carousel gift cards desktop

--- js/owlcarouselmain.js
+++ js/owlcarouselmain.js
@@ -169,4 +169,49 @@ $('.owl-carousel-brands').owlCarousel({
 });
 // FIN OWL CAROUSEL BRANDS
 
+  // OWL CAROUSEL GIFT CARDS
+  $('.owl-carousel-gift-cards').owlCarousel({
+    stagePadding: 40,
+    left: -30,
+    items: 4,
+    loop: true,
+    margin: 16,
+    nav: true,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplayHoverPause:true,
+    navText:["<div class='nav-btn prev-slide'></div>","<div class='nav-btn next-slide'></div>"],
+    dots: false,
+    responsive: {
+      0: {
+        items: 2
+      },
+      360: {
+        items: 2
+      },
+      480: {
+        items: 2
+      },
+      568: {
+        items: 3
+      },
+      736: {
+        items: 3
+      },
+      768: {
+        items: 3
+      },
+      824: {
+        items: 4
+      },
+      1024: {
+        items: 4
+      },
+      1366: {
+        items: 4
+      }
+    }
+  });
+  // FIN OWL CAROUSEL GIFT CARDS
+
 });

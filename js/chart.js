b76394a8bd7576b8676ab0194222ea3de46d4a1b
javierbull <javierbullreyes@gmail.com>
2024-03-29 16:16:22 -0500

    colors chart colaboradores

--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,39 @@
-var ctx = document.getElementById("myChart").getContext("2d");
+var ctx = document.getElementById("chartColaboradores").getContext("2d");
 var myChart = new Chart(ctx, {
     type: "bar",
     data: {
-        labels: ['col1', 'col2', 'col3'],
+        labels: ['Plataforma', 'Inhouse', 'Pre registrados', 'Sin confirmar'],
         datasets: [{
-            label: 'num datos',
-            data: [10, 9, 15]
+            label: false,
+            data: [250, 100, 50, 50],
+            backgroundColor: [
+                'rgba(223, 251, 242, 1)',
+                'rgba(238, 246, 252, 1)',
+                'rgba(255, 251, 235, 1)',
+                'rgba(254, 236, 240, 1)'
+            ],
+            borderColor: [
+                'rgba(0,150,109,1)',
+                'rgba(29, 114, 170, 1)',
+                'rgba(227, 150, 1, 1)',
+                'rgba(204, 15, 53, 1)'
+            ],
+            borderWidth: 2,
+            fill: false,
         }]
-    }
+        
+    },
+    options: {
+        scales: {
+          yAxes: [{
+            ticks:{
+                beginAtZero: true
+            }
+          }]
+        },
+        
+        
+      },
+   
+
 });

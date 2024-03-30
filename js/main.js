1147005f2c4a10b4b106b4e05223bd379df00d8d
javierbull <javierbullreyes@gmail.com>
2024-03-30 15:18:01 -0500

    logo empresa

--- js/main.js
+++ js/main.js
@@ -414,6 +414,20 @@ $('.dtable').DataTable({
 
 // SHOW ACTIVE FOCUS STEPPER
 
+$('#editar-empresa-input-preview-img').change(function(){
+  var curElement = $('#editar-empresa-preview-logo-empresa');
+  console.log(curElement);
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      curElement.attr('src', e.target.result);
+  };
+
+  // read the image file as a data URL.
+  reader.readAsDataURL(this.files[0]);
+});
+
  
 });
 

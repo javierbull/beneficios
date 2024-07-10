1a746cbdd6f15848067770e892cce0ae5346f558
javierbull <javierbullreyes@gmail.com>
2024-07-10 16:25:08 -0500

    base dtable

--- js/admin.js
+++ js/admin.js
@@ -26,5 +26,28 @@ $(document).ready(function () {
     $("#dashboard-panel").hide();
     $("#colaboradores-panel").show();
   });
-  // end show admi panels
+  // end show admib panels
+
+  // DATATABLES
+  $('.dtable').DataTable({
+    // scrollX: true,
+    //para cambiar el lenguaje a español
+    "language": {
+      "lengthMenu": "Mostrar _MENU_ filas",
+      "zeroRecords": "No se encontraron resultados",
+      "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+      "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+      "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+      "sSearch": "Buscar:",
+      "oPaginate": {
+        "sFirst": "Primero",
+        "sLast": "Último",
+        "sNext": "Siguiente",
+        "sPrevious": "Anterior"
+      },
+      "sProcessing": "Procesando...",
+    }
+
+  });
+  // END DATATABLES
 });

7378a2c7c7bd9139eeaed7c9abf8edad5e7304d3
javierbull <javierbullreyes@gmail.com>
2024-07-11 16:31:08 -0500

    cargar logo empresa

--- js/admin.js
+++ js/admin.js
@@ -58,11 +58,23 @@ $(document).ready(function () {
     "info": false,
     "dom": '<"top"i>rt<"bottom"lfp><"clear">',
   });
+  // END DATATABLES
 
+  // CAMBIAR LOGO EMPRESA
+  $('#editar-empresa-input-preview-img').change(function () {
+    var curElement = $('#editar-empresa-preview-logo-empresa');
+    console.log(curElement);
+    var reader = new FileReader();
 
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      curElement.attr('src', e.target.result);
+    };
 
-
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+  });
+  // END CAMBIAR LOGO EMPRESA
 });
 
 
-// END DATATABLES

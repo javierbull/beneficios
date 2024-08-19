b92ec61486a45dbee7fb178c1299929a7cc74cec
javierbull <javierbullreyes@gmail.com>
2024-08-19 14:12:38 -0500

    card planilla base

--- js/admin.js
+++ js/admin.js
@@ -244,6 +244,17 @@ $(document).ready(function () {
   });
   // END CONTADOR DE CARACTERES
 
+  // UPLOAD PAGO PLANILLA
+  $('.fileInput').change(function() {
+    var numfiles = $(this)[0].files.length;
+    var parent = $(this).closest('.input-file');
+    parent.find('ins').remove();
+    for (i = 0; i < numfiles; i++) { 
+      parent.append('<ins>' + $(this)[0].files[i].name + '</ins>')
+    }
+  });
+  // END UPLOAD PAGO PLAMILLA
+
 });
 
 

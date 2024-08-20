25c9730fd56c10f2b06ce3d50307ba2503bcfe7d
javierbull <javierbullreyes@gmail.com>
2024-08-20 10:58:19 -0500

    function select all checkboxes

--- js/admin.js
+++ js/admin.js
@@ -281,6 +281,26 @@ $(document).ready(function () {
   });
   // END UPLOAD PAGO PLAMILLA
 
+  $(function(){
+    $('.checkAll').click(function(){
+       if (this.checked) {
+          $(".checkboxes").prop("checked", true);
+       } else {
+          $(".checkboxes").prop("checked", false);
+       }	
+    });
+  
+    $(".checkboxes").click(function(){
+       var numberOfCheckboxes = $(".checkboxes").length;
+       var numberOfCheckboxesChecked = $('.checkboxes:checked').length;
+       if(numberOfCheckboxes == numberOfCheckboxesChecked) {
+          $(".checkAll").prop("checked", true);
+       } else {
+          $(".checkAll").prop("checked", false);
+       }
+    });
+ });
+
 });
 
 

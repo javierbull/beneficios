b8ad0875e5fa078d7cb18cfe7cc31a6f2a1dc6a9
javierbull <javierbullreyes@gmail.com>
2024-12-05 17:14:26 -0500

    add modal edit banners done

--- js/admin.js
+++ js/admin.js
@@ -96,6 +96,34 @@ $(document).ready(function () {
     // read the image file as a data URL.
     reader.readAsDataURL(this.files[0]);
   });
+
+  $('#input-preview-img-editBannerMobile').change(function () {
+    var curElement = $('#preview-editBannerMobile');
+    console.log(curElement);
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      curElement.attr('src', e.target.result);
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+  });
+
+  $('#input-preview-img-editBannerDesktop').change(function () {
+    var curElement = $('#preview-editBannerDesktop');
+    console.log(curElement);
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      // get loaded data and render thumbnail.
+      curElement.attr('src', e.target.result);
+    };
+
+    // read the image file as a data URL.
+    reader.readAsDataURL(this.files[0]);
+  });
   // END IMAGE PREVIEW
 
   // Sidebar active item

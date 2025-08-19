5e053be58480b318341c73aa4f3deee94475c48c
javierbull <javierbullreyes@gmail.com>
2025-08-19 11:53:46 -0500

    add card dashboard Onboarding con codigo

--- js/adminchart.js
+++ js/adminchart.js
@@ -5,21 +5,21 @@ var myChart = new Chart(ctx, {
         labels: ['Cineplanet', 'Entretenimiento', 'Salud y belleza', 'Boxes y regalos', 'Viajes y turismo'],
         datasets: [{
             label: 'Solicitudes',
-                data: [1000,830,950,450,500],
-                fill: true,
-                borderRadius: 8,
-                backgroundColor: [
-                    '#0ABDA0',
-                    '#59E5C8',
-                    '#24D5B5',
-                    '#96FAE1',
-                    '#CAFEEE'
-                ],
-                datalabels: {
-                    display: false
-                }
+            data: [1000, 830, 950, 450, 500],
+            fill: true,
+            borderRadius: 8,
+            backgroundColor: [
+                '#0ABDA0',
+                '#59E5C8',
+                '#24D5B5',
+                '#96FAE1',
+                '#CAFEEE'
+            ],
+            datalabels: {
+                display: false
+            }
         }]
-        
+
     },
     options: {
         plugins: {
@@ -31,50 +31,14 @@ var myChart = new Chart(ctx, {
             }
         },
         scales: {
-          yAxes: [{
-            ticks:{
-                beginAtZero: true
-            }
-          }]
-        },
-        
-        
-      },
-   
-
-});
-
-var ctx = document.getElementById("chartColaboradores").getContext("2d");
-var myChart = new Chart(ctx, {
-    type: "doughnut",
-    data: {
-        labels: ['Activos', 'Inactivos', 'Pre-registrados'],
-        datasets: [{
-            label: 'Colaboradores',
-                data: [400,350,250],
-                fill: true,
-                backgroundColor: [
-                    '#76B435',
-                    '#FFAE11',
-                    '#FF5F73'
-                ],
-                datalabels: {
-                    display: false
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
                 }
-        }]
-        
-    },
-    options: {
-        plugins: {
-            title: {
-                display: false,
-            },
-            legend: {
-                display: false,
-            }
+            }]
         },
-      },
-   
+
+    },
 
 });
 
@@ -85,18 +49,18 @@ var myChart = new Chart(ctx, {
         labels: ['WhatsApp', 'Digital', 'In-house'],
         datasets: [{
             label: 'Porcentaje',
-                data: [40,35,25],
-                fill: true,
-                backgroundColor: [
-                    '#76B435',
-                    '#518AF9',
-                    '#FFBC3B'
-                ],
-                datalabels: {
-                    display: false
-                }
+            data: [40, 35, 25],
+            fill: true,
+            backgroundColor: [
+                '#76B435',
+                '#518AF9',
+                '#FFBC3B'
+            ],
+            datalabels: {
+                display: false
+            }
         }]
-        
+
     },
     options: {
         plugins: {
@@ -107,7 +71,7 @@ var myChart = new Chart(ctx, {
                 display: false,
             }
         },
-      },
-   
+    },
+
 
 });

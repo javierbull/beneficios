55ed27a0d2fa6cb360e44c75f036a72ea4e7b7aa
javierbull <javierbullreyes@gmail.com>
2025-09-02 20:10:46 -0500

    update card categorias mas compradas dashboard

--- js/adminchart.js
+++ js/adminchart.js
@@ -2,18 +2,19 @@ var ctx = document.getElementById("chartCategorias").getContext("2d");
 var myChart = new Chart(ctx, {
     type: "bar",
     data: {
-        labels: ['Cineplanet', 'Entretenimiento', 'Salud y belleza', 'Boxes y regalos', 'Viajes y turismo'],
+        labels: ['Cineplanet', 'Entretenimiento', 'Salud y belleza', 'Boxes y regalos', 'Viajes y turismo', 'Otros'],
         datasets: [{
             label: 'Solicitudes',
-            data: [1000, 830, 950, 450, 500],
+            data: [900, 850, 800, 780, 500, 550],
             fill: true,
             borderRadius: 8,
             backgroundColor: [
-                '#0ABDA0',
-                '#59E5C8',
-                '#24D5B5',
-                '#96FAE1',
-                '#CAFEEE'
+                '#2F71E5',
+                '#BAD5FF',
+                '#BAD5FF',
+                '#BAD5FF',
+                '#BAD5FF',
+                '#BAD5FF'
             ],
             datalabels: {
                 display: false
